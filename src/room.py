9f50fb43d87cb5dbc91432dea8772af1797b19ac
PJ Wise <pjwise@PJs-MacBook-Pro.attlocal.net>
2020-08-13 18:24:31 -0400

    pushing mvp full

--- src/room.py
+++ src/room.py
@@ -1,2 +1,25 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+        self.items = []
+
+    def __str__(self):
+        return f'Room: {self.name}'
+
+    def __repr__(self):
+        return f'Room({repr(self.name)})'
+    
+    # def add_item(self, item)
+    #     pass
+    
+    # def remove_item(self, item):
+    #     pass
+
